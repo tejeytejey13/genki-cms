c2430eeea86b65a7c5640f4d50262926596dde0f
ichizawa <ryonuzuke@gmail.com>
2024-04-09 19:31:12 +0800

    new

--- main/component/head.php
+++ main/component/head.php
@@ -2,9 +2,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+    <!-- <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
     <meta http-equiv="Pragma" content="no-cache">
-    <meta http-equiv="Expires" content="0">
+    <meta http-equiv="Expires" content="0"> -->
     <title>Genki School Clinic Management System</title>
     <link rel="shortcut icon" href="./img/assets/PNJK PNG.png" />
 
